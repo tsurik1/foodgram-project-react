9756e27c522d3274c459d85ac9d19307fd0babab
yauheni <tsurik_01@mail.ru>
2023-05-22 18:33:06 +0300

    ограничил djoser  урлы пользователей

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
-
+from djoser.views import UserViewSet
 from api.views import (SubscribeView, SubscriptionView,
                        RecipesViewSet, TagViewSet, IngredientViewSet,
                        ShoppingCartView, FavoriteView,
@@ -10,10 +10,18 @@ from api.views import (SubscribeView, SubscriptionView,
 
 router = DefaultRouter()
 
+router.register('users', UserViewSet)
 router.register('recipes', RecipesViewSet, basename='recipes')
 router.register('tags', TagViewSet)
 router.register('ingredients', IngredientViewSet)
 
+djoser_urlpatterns = [
+    path('me/', UserViewSet.as_view({'get': 'me'}), name='me'),
+    path(
+        'set_password/',
+        UserViewSet.as_view({'post': 'set_password/'}), name='set_password/'
+    )
+]
 urlpatterns = [
     path('users/subscriptions/', SubscriptionView.as_view()),
     path('users/<int:pk>/subscribe/', SubscribeView.as_view()),
@@ -22,5 +30,5 @@ urlpatterns = [
     path('recipes/<int:pk>/shopping_cart/', ShoppingCartView.as_view()),
     path('auth/', include('djoser.urls.authtoken')),
     path('', include(router.urls)),
-    path('', include('djoser.urls'))
+    path('users/', include(djoser_urlpatterns))
 ]
