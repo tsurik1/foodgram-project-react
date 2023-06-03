da9a862412f6f210cd43521eb8821054c99a45a8
yauheni <tsurik_01@mail.ru>
2023-06-03 23:41:33 +0300

    filter

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -19,7 +19,7 @@ class RecipeFilter(FilterSet):
 
     def filter_is_favorited(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
-            return queryset.filter(favorite__user=self.request.user)
+            return queryset.filter(in_favorite__user=self.request.user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
