f96ddb9afdfee4f4abd7592dd1e5dabac685bc64
yauheni <tsurik_01@mail.ru>
2023-06-03 13:52:10 +0300

    удаление для админа 2

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -6,7 +6,7 @@ class AuthorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
             request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
+            or request.user.is_authenticated or request.user.is_staff
         )
 
     def has_object_permission(self, request, view, obj):
