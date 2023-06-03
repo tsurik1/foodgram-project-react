418740708e03d3f2c5417dab28fa8ebdeb705077
yauheni <tsurik_01@mail.ru>
2023-06-03 13:29:56 +0300

    админка, разрешение админа

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -12,5 +12,5 @@ class AuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
-            or request.user == obj.author
+            or request.user == obj.author or request.user.is_staff
         )
