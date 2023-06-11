af35b521188a2db29eefe5ada7862d2023b4ee29
yauheni <tsurik_01@mail.ru>
2023-06-12 00:46:27 +0300

    12345

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -7,9 +7,10 @@ from .models import Subscription, User
 class UsersAdmin(admin.ModelAdmin):
     list_display = ('pk', 'username')
     search_fields = ('email', 'username',)
-    readonly_fields = ('password', 'email', 'username')
+    readonly_fields = ('username', 'email', 'password',)
 
 
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
     list_display = ('pk', 'subscriber', 'subscription',)
+    ordering = ('subscriber', 'subscription',)
